6b05ee26a470de51756e940513989afa47505bf2
Saihc <593746412@qq.com>
2019-01-15 01:00:51 +0800

    mpv0.2-designTab

--- miniprogram/pages/design/design.js
+++ miniprogram/pages/design/design.js
@@ -1,66 +1,87 @@
 // miniprogram/pages/design/design.js
 Page({
 
-	/**
-	 * 页面的初始数据
-	 */
-	data: {
+   /**
+    * 页面的初始数据
+    */
+   data: {
 
-	},
+   },
 
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
-	onLoad: function (options) {
+   /**
+    * 生命周期函数--监听页面加载
+    */
+   onLoad: function(options) {
 
-	},
+   },
 
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
+   /**
+    * 生命周期函数--监听页面初次渲染完成
+    */
+   onReady: function() {
 
-	},
+   },
 
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
+   /**
+    * 生命周期函数--监听页面显示
+    */
+   onShow: function() {
 
-	},
+   },
 
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
+   /**
+    * 生命周期函数--监听页面隐藏
+    */
+   onHide: function() {
 
-	},
+   },
 
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
+   /**
+    * 生命周期函数--监听页面卸载
+    */
+   onUnload: function() {
 
-	},
+   },
 
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
+   /**
+    * 页面相关事件处理函数--监听用户下拉动作
+    */
+   onPullDownRefresh: function() {
 
-	},
+   },
 
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
+   /**
+    * 页面上拉触底事件的处理函数
+    */
+   onReachBottom: function() {
 
-	},
+   },
 
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
+   /**
+    * 用户点击右上角分享
+    */
+   onShareAppMessage: function() {
 
-	}
+   },
+
+   gotoDetail(e) {
+      let name = e.currentTarget.dataset.name;
+      switch (name) {
+         case "ang":
+            wx.navigateTo({
+               url: './../detail-ang/detail-ang',
+            })
+            break;
+         case "jjtg":
+            wx.navigateTo({
+					url: './../detail-jjtg/detail-jjtg',
+            })
+            break;
+         case "jmf":
+            wx.navigateTo({
+					url: './../detail-jmf/detail-jmf',
+            })
+            break;
+      }
+   }
 })
